9583d8c16e5c9f81882b91f833b6bb40fa329a94
Pete Taylor <pete.jc.taylor@hey.com>
2021-11-05 13:40:45 -0400

    bug fix for sortable column logic

--- app/graphql/tasker/queries/helpers.rb
+++ app/graphql/tasker/queries/helpers.rb
@@ -21,8 +21,8 @@ module Tasker
       sig { params(model: T.untyped, limit: T.nilable(Integer), offset: T.nilable(Integer), sort_by: T.nilable(T.any(String, Symbol)), sort_order: T.nilable(T.any(String, Symbol))).returns(T::Hash[Symbol, T.any(Integer, String, Symbol)]) }
       def page_sort_params(model, limit, offset, sort_by, sort_order)
         valid_sorts = model.column_names.map(&:to_sym)
-        sort_by = :created_at unless valid_sorts.include?(sort_by)
-        sort_order = :asc unless %i[asc desc].include?(sort_order)
+        sort_by = :created_at unless valid_sorts.include?(sort_by.to_s.to_sym)
+        sort_order = :asc unless %i[asc desc].include?(sort_order.to_s.to_sym)
         order = { sort_by => sort_order }
         { limit: (limit || 20).to_i, offset: (offset || 0).to_i, order: order }
       end
