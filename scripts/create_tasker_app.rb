a5938b1bf9496fa1807839e9480b63847955483d
Pete Taylor <pete.jc.taylor@hey.com>
2025-07-10 13:56:08 -0400

    fix: updated version in installer

--- scripts/create_tasker_app.rb
+++ scripts/create_tasker_app.rb
@@ -24,7 +24,7 @@ require 'active_support/core_ext/string'
 
 class TaskerAppGenerator < Thor
   TEMPLATES_DIR = File.expand_path('templates', __dir__)
-  TASKER_VERSION = '~> 2.6.1' # Use latest published version
+  TASKER_VERSION = '~> 1.0.4' # Use latest published version
 
   # Fix Thor deprecation warning
   def self.exit_on_failure?
